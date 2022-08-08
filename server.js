2ee32337542815c7190f8184b4985fb4b0917ba6
Magdalena BenBassatLuszczynska <magdalena@a-i.com>
2022-08-08 13:39:39 +0300

    fixing .env

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 /** @format */
 
-const dotenv = require('dotenv').config();
-const express = require('express');
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
 const app = express();
-const PORT = 8000;
-const petRoutes = require('./routes/petRoute');
-const userRoutes = require('./routes/userRoute');
-const authRoutes = require('./routes/authRoute');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const { imageMiddleware } = require('./middleware');
+const petRoutes = require("./routes/petRoute");
+const userRoutes = require("./routes/userRoute");
+const authRoutes = require("./routes/authRoute");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { imageMiddleware } = require("./middleware");
 
-const User = require('./models/User');
-const ImageKit = require('imagekit');
+const User = require("./models/User");
+const ImageKit = require("imagekit");
 
 const {
+  PORT,
   SECRET_KEY,
   MONGO_URI,
   IMAGEKIT_ENDPOINT,
   IMAGEKIT_PUBLICKEY,
   IMAGEKIT_PRIVATEKEY,
 } = process.env;
-
-const { MongoClient } = require('mongodb');
+console.log(MONGO_URI);
+const { MongoClient } = require("mongodb");
 const imagekit = new ImageKit({
   urlEndpoint: IMAGEKIT_ENDPOINT,
   publicKey: IMAGEKIT_PUBLICKEY,
@@ -33,13 +34,13 @@ const url = MONGO_URI;
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use('/pet', petRoutes);
-app.use('/user', userRoutes);
-app.use('/auth', authRoutes);
-app.use('*', (req, res) => {
-  res.status(404).send('Page not found');
+app.use("/pet", petRoutes);
+app.use("/user", userRoutes);
+app.use("/auth", authRoutes);
+app.use("*", (req, res) => {
+  res.status(404).send("Page not found");
 });
-app.get('/auth', imageMiddleware, (req, res) => {
+app.get("/auth", imageMiddleware, (req, res) => {
   const result = imagekit.getAuthenticationParameters();
   res.send(result);
 });
